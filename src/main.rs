740434b972a6dd494b6fd7af5be20b8d87a1ebd9
Zachary Rivait <zchryrvt@gmail.com>
2020-11-30 21:38:19 -0500

    grabs the tag structs from the flacs in the current directory

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,75 @@
 use std::{fs, env};
+use std::path::{Path, PathBuf};
+use metaflac::Tag;
 
 fn main() {
 
-    let args = env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
+    let pwd = match env::current_dir() {
+
+        Ok(x) => x,
+        Err(_) => panic!("could not get working directory"),
+
+    };
+
+    println!("pwd:{:?}", pwd);
+
+    let flacs = get_flacs_sorted(pwd);
+    let mut tags = Vec::new();
+
+    for flac in flacs {
+
+        let tag = match Tag::read_from_path(flac) {
+
+            Ok(x) => x,
+            Err(_) => panic!("error reading flac"),
+
+        };
+
+        tags.push(tag);
+
+    }
+
+}
+
+// gets the flacs in the given directory
+// passes them back as a sorted vec 
+fn get_flacs_sorted(pwd: PathBuf) -> Vec<PathBuf> {
+
+    let mut flacs: Vec<PathBuf> = Vec::new();
+
+    let dir = match fs::read_dir(pwd) {
+    
+        Ok(x) => x,
+        Err(_) => panic!("error opening directory")
+
+    };
+
+    for entry in dir {
+
+        let file = match entry{
+
+            Ok(x) => x,
+            Err(_) => panic!("io error")
+
+        };
+        
+        let path = file.path();
+
+        let is_flac = match path.extension() {
+
+            Some(x) => x == "flac",
+            None => false
+
+        };
+
+        if is_flac {
+            flacs.push(path);
+        }
+    }
+
+    flacs.sort();
+
+    flacs
 }
