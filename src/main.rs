b1e6923a777fa59c9cb38f391fa105d975c7234f
Zachary Rivait <zchryrvt@gmail.com>
2020-12-31 22:30:48 -0500

    preliminary work on flags

--- src/main.rs
+++ src/main.rs
@@ -26,14 +26,28 @@ fn main() {
     };
 
     let mut format = String::new();
-    
+
+    let mut outfile = String::from("tags.txt");
+
+    let mut unsupported_tags = false;
+
     // process the other arguments
     for arg in args {
 
         if arg.starts_with('-') {
             match arg.as_str() {
 
-                "-o" | "--out" => (),
+                "-o" | "--out" => {
+
+                    match args.next() {
+
+                        Some(x) => outfile = String::from(x),
+                        None => {
+                            println!("invalid argument");
+                            process::exit(1);
+                        }
+                    }
+                }
                 "-i" | "--in" => (),
                 "-u" | "--unsupported-tags" => (),
                 _ => (),
@@ -56,11 +70,13 @@ fn main() {
     }
 
     // panics if given unsupported tags
-    if !common::is_supported_tags(&format) {
+    if !unsupported_tags {
+        if !common::is_supported_tags(&format) {
 
-        println!("Unsupported tags found in format specifier. Please pass the --unsupported-tags option. Exiting");
-        process::exit(1);
+            println!("Unsupported tags found in format specifier. Please pass the --unsupported-tags option. Exiting");
+            process::exit(1);
 
+        }
     }
 
     let pwd = match env::current_dir() {
@@ -105,6 +121,3 @@ fn main() {
         }
     }
 }
-
-
-
